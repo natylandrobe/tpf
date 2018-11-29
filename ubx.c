e8d5a91c9b9a34ffa9b76598c7a286a39beb771b
waisburd <waisburdazar@gmail.com>
2018-11-29 17:58:32 -0300

    Se agregan funciones para procesar la fecha de las sentencias

--- ubx.c
+++ ubx.c
@@ -26,13 +26,31 @@
 #define CKA_IND 0
 #define CKB_IND 1
 
-typedef enum{S_EPTNULL, S_ENOMEM, S_EREAD, S_CLASS_INV, S_ID_INV, S_CK_INV, S_LARGO_INV, S_OK} ubxst_t;
-unsigned int calc_largo(char info[]);
-ubxst_t procesar_ubx(FILE *fin);
-ubxst_t ubx_cksum(char *ckBuff, int n, FILE *fin);
+//defines para calc_fecha
+#define P_ANIO_MBIND 9
+#define P_ANIO_LBIND 8
+#define PVT_MOIND 10
+#define PVT_DIND 11
+#define PVT_HIND 12
+#define PVT_MININD 13
+#define PVT_SEGIND 14
+#define T_ANIO_MBIND 13
+#define T_ANIO_LBIND 12
+#define TT_MOIND 14
+#define TT_DIND 15
+#define TT_HIND 16
+#define TT_MININD 17
+#define TT_SEGIND 18
 
-ubxst_t procesar_ubx(FILE *fin){
-	char info_largo[LARGO_CK_SZ], *buff;
+typedef enum{S_EPTNULL, S_ENOMEM, S_EREAD, S_CLASS_INV, S_ID_INV, S_CK_INV, S_LARGO_INV, S_OK} ubxst_t;
+unsigned int calc_largo(unsigned char info[]);
+ubxst_t procesar_ubx(FILE *fin, struct fecha *fecha);
+ubxst_t ubx_cksum(unsigned char *ckBuff, int n, FILE *fin);
+ubxst_t calc_fecha(unsigned char *buff, struct fecha *fecha, unsigned char id);
+ubxst_t cargar_fecha(void *dato, struct fecha *funi, unsigned char id, unsigned char *buff, ubxst_t (*proc_fecha)(unsigned char *, struct fecha *, unsigned char));
+
+ubxst_t procesar_ubx(FILE *fin, struct fecha *fecha){
+	unsigned char info_largo[LARGO_CK_SZ], *buff;
 	unsigned int c, id, largo;
 	ubxst_t cks;
 
@@ -143,16 +161,83 @@ ubxst_t procesar_ubx(FILE *fin){
 			}
 	}//cierra switch
 
+
 		
 
 }
 
-unsigned int calc_largo(char info[]){
+
+ubxst_t cargar_fecha(void *dato, struct fecha *funi, unsigned char id, unsigned char *buff, ubxst_t (*proc_fecha)(unsigned char *, struct fecha *, unsigned char)){
+	struct s_PVT * pvt_s;
+	struct s_TIM_TOS * tt_s;
+	struct s_POSLLH * posllh_s;
+
+	if(!dato || !funi || !buff || !proc_fecha){
+		return S_EPTNULL;
+	}
+
+	switch(id){
+		case PVT_ID:
+			pvt_s = (struct s_PVT *)dato;
+
+			if((*proc_fecha)(buff, &(pvt_s->f), id) != S_OK){
+				return S_EPTNULL;
+			}
+
+			*funi = pvt_s->f;
+			break;
+		case TIM_TOS_ID:
+			tt_s  = (struct s_TIM_TOS *)dato;
+			if((*proc_fecha)(buff, &(tt_s->f), id) != S_OK){
+				return S_EPTNULL;
+			}
+
+			*funi = tt_s->f;
+			break;
+		case POSLLH_ID:
+			posllh_s = (struct s_POSLLH *)dato;
+
+			posllh_s->f = *funi;
+			break;
+	}
+
+	return S_OK;
+}
+
+ubxst_t calc_fecha(unsigned char *buff, struct fecha *fecha, unsigned char id){
+
+	if(!buff || !fecha){
+		return S_EPTNULL;
+	}
+
+	switch(id){
+		case PVT_ID:
+			fecha->anio = ((buff[P_ANIO_MBIND] & MASK_LE) << 8)|((buff[P_ANIO_LBIND] & MASK_LE));
+			fecha->mes = buff[PVT_MOIND];
+			fecha->dia = buff[PVT_DIND];
+			fecha->hora = buff[PVT_HIND];
+			fecha->minutos = buff[PVT_MININD];
+			fecha->segundos = buff[PVT_SEGIND];
+			break;
+		case TIM_TOS_ID:
+			fecha->anio = ((buff[T_ANIO_MBIND] & MASK_LE) << 8)|((buff[T_ANIO_LBIND] & MASK_LE));
+			fecha->mes = buff[TT_MOIND];
+			fecha->dia = buff[TT_DIND];
+			fecha->hora = buff[TT_HIND];
+			fecha->minutos = buff[TT_MININD];
+			fecha->segundos = buff[TT_SEGIND];
+			break;
+	}
+
+	return S_OK;
+}
+
+unsigned int calc_largo(unsigned char info[]){
 	return ((info[MSB_IND] & MASK_LE) << 8)|((info[LSB_IND] & MASK_LE));
 }
 
 
-ubxst_t ubx_cksum(char *ckBuff, int n, FILE *fin){
+ubxst_t ubx_cksum(unsigned char *ckBuff, int n, FILE *fin){
 
 	unsigned char ck_a = 0;
 	unsigned char ck_b = 0;
