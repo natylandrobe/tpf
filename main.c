558af426ef2cc5fddf50b6618b26cd390b302fd4
Michael <mike45831@hotmail.com>
2018-11-30 13:13:44 -0300

    Modifique el main, para que depende el protocolo haga lo del nmea o ubx

--- main.c
+++ main.c
@@ -90,55 +90,63 @@ FILE *fin, *fout, *flog;
 		}
 	}
 
+	if (arg.protocol==NMEA){
 	// este while es para hacer pruebas, no es parte del programa
-	while(fgets(linea, MAX_LINE, fin)){
+		while(fgets(linea, MAX_LINE, fin)){
 
-		fprintf(fout, "%s", linea);
+			fprintf(fout, "%s", linea);
 
-		t=checkLine(linea);
-		switch (t){
-		case GGA : if ( cargar_struct_gga(linea,&Gga,&fecha)){
+			t=checkLine(linea);
+			switch (t){
+			case GGA : if ( cargar_struct_gga(linea,&Gga,&fecha)){
 			//para probar
-		printf("%s\n","BIENGGA");
-		printf("%.3f\n",Gga.lat);
-		printf("%.3f\n",Gga.lon);
-		printf("%.1f\n",Gga.ele);
-		printf("%.1f\n",Gga.hdop);
-		printf("%.1f\n",Gga.separacion);
-		printf("%li\n",Gga.cantSat);
-		printf("%i\n",fecha.hora);
-		printf("%i\n",fecha.minutos);
-		printf("%.3f\n",fecha.segundos);
+			printf("%s\n","BIENGGA");
+			printf("%.3f\n",Gga.lat);
+			printf("%.3f\n",Gga.lon);
+			printf("%.1f\n",Gga.ele);
+			printf("%.1f\n",Gga.hdop);
+			printf("%.1f\n",Gga.separacion);
+			printf("%li\n",Gga.cantSat);
+			printf("%i\n",fecha.hora);
+			printf("%i\n",fecha.minutos);
+			printf("%.3f\n",fecha.segundos);
 							} break;
-		case RMC :
-		if (cargar_struct_rmc(linea,&Rmc,&fecha)){
+			case RMC :
+			if (cargar_struct_rmc(linea,&Rmc,&fecha)){
 		//para probar
-		printf("%s\n","BIENRM");
-		printf("%.3f\n",Rmc.lat);
-		printf("%.3f\n",Rmc.lon);
-		printf("%c\n",Rmc.status);
-		printf("%i\n",fecha.hora);
-		printf("%i\n",fecha.minutos);
-		printf("%.3f\n",fecha.segundos);							}
+			printf("%s\n","BIENRM");
+			printf("%.3f\n",Rmc.lat);
+			printf("%.3f\n",Rmc.lon);
+			printf("%c\n",Rmc.status);
+			printf("%i\n",fecha.hora);
+			printf("%i\n",fecha.minutos);
+			printf("%.3f\n",fecha.segundos);							}
 						 break;
-		case ZDA :
-		if(cargar_struct_zda(linea,&Zda,&fecha)){
+			case ZDA :
+			if(cargar_struct_zda(linea,&Zda,&fecha)){
 			//para probar
-		printf("%s\n","BIENZD");
-		printf("%i\n",fecha.dia);
-		printf("%i\n",fecha.mes);
-		printf("%i\n",fecha.anio);
-		printf("%i\n",fecha.hora);
-		printf("%i\n",fecha.minutos);
-		printf("%.3f\n",fecha.segundos);
-		printf("%i\n",Zda.time_zone);
-		printf("%i\n",Zda.dif_tmzone);
+			printf("%s\n","BIENZD");
+			printf("%i\n",fecha.dia);
+			printf("%i\n",fecha.mes);
+			printf("%i\n",fecha.anio);
+			printf("%i\n",fecha.hora);
+			printf("%i\n",fecha.minutos);
+			printf("%.3f\n",fecha.segundos);
+			printf("%i\n",Zda.time_zone);
+			printf("%i\n",Zda.dif_tmzone);
 				 }
 				break;
-		case NING: printf("%s\n","MAL");
+				case NING: printf("%s\n","MAL");
+		
+			}
 		
 		}
-		
+	}
+
+	else if(arg.protocol==UBX){
+		//aca iria lo de ubx
+		printf("%s\n","UBX");
+
 	}
 
 	printf("name: %s\n", arg.name);
